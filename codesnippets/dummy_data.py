a655e5c07488a3640927f71bb3f6ac1b23272544
jimdevops19 <jimdevops@gmail.com>
2021-08-14 13:01:27 +0300

    PublishAll

---
+++ codesnippets/dummy_data.py
@@ -0,0 +1,18 @@
+dummy_data = [
+    {
+        "id": 1,
+        "starting_time": "16:00",
+        "team_a": "Random Team 1",
+        "score": "0 - 0",
+        "team_b": "Random Team 2",
+        "minute": "00:00",
+    },
+    {
+        "id": 2,
+        "starting_time": "18:00",
+        "team_a": "Random Team 3",
+        "score": "0 - 0",
+        "team_b": "Random Team 4",
+        "minute": "00:00",
+    },
+]
