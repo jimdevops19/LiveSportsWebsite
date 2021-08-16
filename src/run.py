9b59c3b199d2cd676d7e698e648a470a74142589
jimdevops19 <jimdevops@gmail.com>
2021-08-16 13:31:39 +0300

    PublishAll

--- src/run.py
+++ src/run.py
@@ -2,37 +2,21 @@ from flask import Flask
 from flask import render_template
 from flask import jsonify
 from utils import get_ft_time
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/info_json")
 def info_json():
-    dummy_data = [
-        {
-            "id" : 1,
-            "starting_time" : "00:00",
-            "team_a" : "Random Team 1",
-            "score" : "0 - 0",
-            "team_b" : "Random Team 2",
-            "minute" : get_ft_time('00:00'),
-        },
-        {
-            "id" : 2,
-            "starting_time" : "00:05",
-            "team_a" : "Random Team 3",
-            "score" : "0 - 0",
-            "team_b" : "Random Team 4",
-            "minute" : get_ft_time('00:05'),
-        },
-        {
-            "id": 3,
-            "starting_time": "00:00",
-            "team_a": "Random Team 5",
-            "score": "0 - 0",
-            "team_b": "Random Team 6 ",
-            "minute" : get_ft_time('00:06'),
-        },
-    ]
+    with open('dummy_data.json', 'r') as f:
+        dummy_data = json.load(f)
+
+    # Overrides:
+    for item in dummy_data:
+        if item.get('minute') == '':
+            item['minute'] = get_ft_time(item.get('starting_time'))
+
+
     return jsonify(dummy_data)
 
 @app.route("/")
